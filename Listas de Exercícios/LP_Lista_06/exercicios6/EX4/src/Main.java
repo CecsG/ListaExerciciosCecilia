46644ff8eda645d72ecdb23047339fd53b62a33d
CecsG <ceciliageorgeto@gmail.com>
2025-06-12 18:09:50 -0300

    lista 6 completa

---
+++ Listas de Exercícios/LP_Lista_06/exercicios6/EX4/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String word;
+
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter your word: ");
+            word = in.next();
+        } while (word.isEmpty());
+
+        String lowerCase = word.toLowerCase();
+        char[] array = lowerCase.toCharArray();
+
+        if (verifyPalindrome(array, 0, array.length-1)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        in.close();
+    }
+
+    public static boolean verifyPalindrome(char[] array, int start, int end) {
+        while (start < end) {
+            if (array[start] != array[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
